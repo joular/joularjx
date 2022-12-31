b82d62ec221ddb2bf812bcb7569e232c643ab196
Mark van Wijk <m.g.w.vanwijk@student.utwente.nl>
2022-12-31 18:02:10 +0100

    Unnest the CpuFactory

--- src/main/java/org/noureddine/joularjx/cpu/CpuFactory.java
+++ src/main/java/org/noureddine/joularjx/cpu/CpuFactory.java
@@ -37,49 +37,44 @@ public class CpuFactory {
      */
     public static Cpu getCpu(final AgentProperties properties) {
         // Get OS
-        final String osName = System.getProperty("os.name").toLowerCase();
-        final String osArch = System.getProperty("os.arch").toLowerCase();
+        String osName = System.getProperty("os.name").toLowerCase();
+        String osArch = System.getProperty("os.arch").toLowerCase();
 
-        if (osName.contains("linux")) {
-            // GNU/Linux
-            if (osArch.contains("aarch64") || osArch.contains("arm")) {
-                // Check if Raspberry Pi and use formulas
-                final Optional<RaspberryPiModels> rPiModelName = getRPiModelName(osArch);
-                if (rPiModelName.isPresent()) {
-                    return new RaspberryPi(rPiModelName.get());
-                } else {
-                    // Platform not supported
-                    logger.log(Level.SEVERE, "Platform not supported. Exiting...");
-                    System.exit(1);
-                }
-            } else {
-                // Suppose it's x86/64, check for powercap RAPL
-                try {
-                    String raplFolderPath = "/sys/class/powercap/intel-rapl/intel-rapl:0";
-                    File raplFolder = new File(raplFolderPath);
-                    if (raplFolder.exists()) {
-                        // Rapl is supported
-                        return new RaplLinux();
-                    } else {
-                        // If no RAPL, then no support
-                        logger.log(Level.SEVERE, "Platform not supported. Exiting...");
-                        System.exit(1);
-                    }
-                } catch (Exception e) {
-                    // If no RAPL, then no support
-                    logger.log(Level.SEVERE, "Platform not supported. Exiting...");
-                    System.exit(1);
-                }
-            }
-        } else if (osName.contains("win")) {
-            // Windows
-            // Check for Intel Power Gadget, and PowerJoular Windows
+        if (osName.contains("win")) {
             return new IntelWindows(properties.getPowerMonitorPath());
-        } else {
-            // Other platforms not supported
+        }
+
+        if (!osName.contains("linux")) {
             logger.log(Level.SEVERE, "Platform not supported. Exiting...");
             System.exit(1);
         }
+
+        if (osArch.contains("aarch64") || osArch.contains("arm")) {
+            return new RaspberryPi(getRPiModelName(osArch).orElseGet(() -> {
+                logger.log(Level.SEVERE, "Platform not supported. Exiting...");
+                System.exit(1);
+                return null;
+            }));
+        }
+
+        // Suppose it's x86/64, check for powercap RAPL
+        try {
+            String raplFolderPath = "/sys/class/powercap/intel-rapl/intel-rapl:0";
+            File raplFolder = new File(raplFolderPath);
+            if (raplFolder.exists()) {
+                // Rapl is supported
+                return new RaplLinux();
+            } else {
+                // If no RAPL, then no support
+                logger.log(Level.SEVERE, "Platform not supported. Exiting...");
+                System.exit(1);
+            }
+        } catch (Exception e) {
+            // If no RAPL, then no support
+            logger.log(Level.SEVERE, "Platform not supported. Exiting...");
+            System.exit(1);
+        }
+
         // Should never reach here because we stop the agent. But the compiler needs the return to compile the code.
         return null;
     }
@@ -92,49 +87,50 @@ public class CpuFactory {
     private static Optional<RaspberryPiModels> getRPiModelName(String osArch) {
         String deviceTreeModel = "/proc/device-tree/model";
         File deviceTreeModelFile = new File(deviceTreeModel);
+        if (!deviceTreeModelFile.exists()) {
+            return Optional.empty();
+        }
 
-        if (deviceTreeModelFile.exists()) {
-            Path procstatPath = Path.of(deviceTreeModel);
-            try {
-                // Read only first line of stat file
-                // We need to read values at index 1, 2, 3 and 4 (assuming index starts at 0)
-                // Example of line: cpu  83141 56 28074 2909632 3452 10196 3416 0 0 0
-                // Split the first line over spaces to get each column
-                List<String> allLines = Files.readAllLines(procstatPath);
-                for (String currentLine : allLines) {
-                    if (currentLine.contains("Raspberry Pi 400 Rev 1.0")) {
-                        if (osArch.contains("aarch64")) {
-                            return Optional.of(RaspberryPiModels.RPI_400_10_64);
-                        }
-                    }
-                    if (currentLine.contains("Raspberry Pi 4 Model B Rev 1.2")) {
-                        if (osArch.contains("aarch64")) {
-                            return Optional.of(RaspberryPiModels.RPI_4B_12_64);
-                        } else {
-                            return Optional.of(RaspberryPiModels.RPI_4B_12);
-                        }
-                    } else if (currentLine.contains("Raspberry Pi 4 Model B Rev 1.1")) {
-                        if (osArch.contains("aarch64")) {
-                            return Optional.of(RaspberryPiModels.RPI_4B_11_64);
-                        } else {
-                            return Optional.of(RaspberryPiModels.RPI_4B_11);
-                        }
-                    } else if (currentLine.contains("Raspberry Pi 3 Model B Plus Rev 1.3")) {
-                        return Optional.of(RaspberryPiModels.RPI_3BP_13);
-                    } else if (currentLine.contains("Raspberry Pi 3 Model B Rev 1.2")) {
-                        return Optional.of(RaspberryPiModels.RPI_3B_12);
-                    } else if (currentLine.contains("Raspberry Pi 2 Model B Rev 1.1")) {
-                        return Optional.of(RaspberryPiModels.RPI_2B_11);
-                    } else if (currentLine.contains("Raspberry Pi Model B Plus Rev 1.2")) {
-                        return Optional.of(RaspberryPiModels.RPI_1BP_12);
-                    } else if (currentLine.contains("Raspberry Pi Model B Rev 2")) {
-                        return Optional.of(RaspberryPiModels.RPI_1B_2);
-                    } else if (currentLine.contains("Raspberry Pi Zero W Rev 1.1")) {
-                        return Optional.of(RaspberryPiModels.RPI_ZW_11);
+        Path procstatPath = Path.of(deviceTreeModel);
+        try {
+            // Read only first line of stat file
+            // We need to read values at index 1, 2, 3 and 4 (assuming index starts at 0)
+            // Example of line: cpu  83141 56 28074 2909632 3452 10196 3416 0 0 0
+            // Split the first line over spaces to get each column
+            List<String> allLines = Files.readAllLines(procstatPath);
+            for (String currentLine : allLines) {
+                if (currentLine.contains("Raspberry Pi 400 Rev 1.0")) {
+                    if (osArch.contains("aarch64")) {
+                        return Optional.of(RaspberryPiModels.RPI_400_10_64);
                     }
                 }
-            } catch (IOException ignored) {}
-        }
+                if (currentLine.contains("Raspberry Pi 4 Model B Rev 1.2")) {
+                    if (osArch.contains("aarch64")) {
+                        return Optional.of(RaspberryPiModels.RPI_4B_12_64);
+                    } else {
+                        return Optional.of(RaspberryPiModels.RPI_4B_12);
+                    }
+                } else if (currentLine.contains("Raspberry Pi 4 Model B Rev 1.1")) {
+                    if (osArch.contains("aarch64")) {
+                        return Optional.of(RaspberryPiModels.RPI_4B_11_64);
+                    } else {
+                        return Optional.of(RaspberryPiModels.RPI_4B_11);
+                    }
+                } else if (currentLine.contains("Raspberry Pi 3 Model B Plus Rev 1.3")) {
+                    return Optional.of(RaspberryPiModels.RPI_3BP_13);
+                } else if (currentLine.contains("Raspberry Pi 3 Model B Rev 1.2")) {
+                    return Optional.of(RaspberryPiModels.RPI_3B_12);
+                } else if (currentLine.contains("Raspberry Pi 2 Model B Rev 1.1")) {
+                    return Optional.of(RaspberryPiModels.RPI_2B_11);
+                } else if (currentLine.contains("Raspberry Pi Model B Plus Rev 1.2")) {
+                    return Optional.of(RaspberryPiModels.RPI_1BP_12);
+                } else if (currentLine.contains("Raspberry Pi Model B Rev 2")) {
+                    return Optional.of(RaspberryPiModels.RPI_1B_2);
+                } else if (currentLine.contains("Raspberry Pi Zero W Rev 1.1")) {
+                    return Optional.of(RaspberryPiModels.RPI_ZW_11);
+                }
+            }
+        } catch (IOException ignored) {}
 
         return Optional.empty();
     }
