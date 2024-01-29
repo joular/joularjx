5dcadbc0bd4c71ea3727aeac83ae52b935371653
Adel Noureddine <adelnoureddine@users.noreply.github.com>
2024-01-29 15:51:51 +0100

    Preparing 2.8.2 release

--- src/main/java/org/noureddine/joularjx/cpu/PowermetricsMacOS.java
+++ src/main/java/org/noureddine/joularjx/cpu/PowermetricsMacOS.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2021-2024, Adel Noureddine, Université de Pau et des Pays de l'Adour.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the
+ * GNU General Public License v3.0 only (GPL-3.0-only)
+ * which accompanies this distribution, and is available at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ */
+
 package org.noureddine.joularjx.cpu;
 
 import org.noureddine.joularjx.utils.JoularJXLogging;
