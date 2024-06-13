765f5bad5d00170bdf5a37e8d39dc2d22c945ec4
Adel Noureddine <adelnoureddine@users.noreply.github.com>
2024-06-13 16:30:02 +0200

    Updates to VM support

--- src/main/java/org/noureddine/joularjx/cpu/VirtualMachine.java
+++ src/main/java/org/noureddine/joularjx/cpu/VirtualMachine.java
@@ -1,12 +1,44 @@
 package org.noureddine.joularjx.cpu;
 
+import org.noureddine.joularjx.utils.JoularJXLogging;
+import java.util.logging.Logger;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.FileSystems;
+import java.nio.file.FileSystem;
+
 public class VirtualMachine implements Cpu {
     
-    @Override
-    public void initialize() {
-        
+    private static final Logger logger = JoularJXLogging.getLogger();
+
+    private static String VM_POWER_PATH_NAME;
+
+    private static String VM_POWER_FORMAT;
+
+    private Path VM_POWER_PATH;
+
+    private final FileSystem fileSystem;
+
+    public VirtualMachine() {
+        this(FileSystems.getDefault());
     }
 
+    public VirtualMachine(final FileSystem fileSystem) {
+        this.fileSystem = fileSystem;
+    }
+
+    @Override
+    public void initialize() {
+        // initialize VM_POWER_PATH and VM_POWER_FORMAT
+        this.VM_POWER_PATH = fileSystem.getPath(VM_POWER_PATH_NAME);
+    }
+
+    /**
+     * The power is approximated based on the CPU load, so it does not need an offset.
+     *
+     * @return 0
+     */
     @Override
     public double getInitialPower() {
         return 0;
@@ -14,9 +46,20 @@ public class VirtualMachine implements Cpu {
 
     @Override
     public double getCurrentPower(double cpuLoad) {
-        return 0;
+        double powerData = 0.0;
+
+        try {
+            powerData += Double.parseDouble(Files.readString(VM_POWER_PATH));
+        } catch (IOException exception) {
+            logger.throwing(getClass().getName(), "getCurrentPower", exception);
+        }
+
+        return powerData;
     }
     
+    /**
+     * Nothing to do here. Method only useful for RAPL
+     */
     @Override
     public double getMaxPower(double cpuLoad) {
         return 0;
