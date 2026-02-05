5cb19503647b43ce6085635c83f3cc1ba400773a
Adel Noureddine <adelnoureddine@users.noreply.github.com>
2026-02-05 12:36:48 +0100

    Revert threadID()/getID(). Update pom.xml.

--- src/main/java/org/noureddine/joularjx/monitor/MonitoringHandler.java
+++ src/main/java/org/noureddine/joularjx/monitor/MonitoringHandler.java
@@ -188,7 +188,7 @@ public class MonitoringHandler implements Runnable {
 		// first compute the proportion of cpu time for each thread in the last sampling
 		// period
 		for (Entry<Thread, Map<String, Integer>> threadEntry : methodsStats.entrySet()) {
-			long threadId = threadEntry.getKey().threadId();
+			long threadId = threadEntry.getKey().getId(); // Is dprecated in Java 19, use threadId() instead
 			long currentThreadCpuTime = threadBean.getThreadCpuTime(threadId);
 			long previousThreadCpuTime = threadsCpuTime.getOrDefault(threadId, 0l);
 			if (currentThreadCpuTime < 0) { // thread has quit
@@ -242,8 +242,7 @@ public class MonitoringHandler implements Runnable {
                     filteredCallTreeStats = extractCallTreesStats(samples, properties::filtersMethod);
                 }
 
-                double cpuLoad = osBean.getSystemCpuLoad(); // In future when Java 17 becomes widely deployed, use
-                                                            // getCpuLoad() instead
+                double cpuLoad = osBean.getSystemCpuLoad(); // If Java >= 14 is available, use the new getCpuLoad() instead
                 double processCpuLoad = osBean.getProcessCpuLoad();
 
                 double energyAfter = cpu.getCurrentPower(cpuLoad);
@@ -374,7 +373,7 @@ public class MonitoringHandler implements Runnable {
 
         for (var statEntry : stats.entrySet()) {
             for (var entry : statEntry.getValue().entrySet()) {
-                double power = threadCpuTimePercentages.get(statEntry.getKey().threadId()) * (entry.getValue() / 100.0);
+                double power = threadCpuTimePercentages.get(statEntry.getKey().getId()) * (entry.getValue() / 100.0);
                 for (ResultWriter resultWriter : resultWriters) {
                     resultWriter.write(entry.getKey().toString(), power);
                 }
@@ -406,7 +405,7 @@ public class MonitoringHandler implements Runnable {
             for (var methodEntry : threadEntry.getValue().entrySet()) {
                 double methodPower = 0.0;
                 if(totalEncounters >= Double.MIN_VALUE) {
-                    methodPower = threadCpuTimePercentages.get(threadEntry.getKey().threadId()) * (methodEntry.getValue() / totalEncounters);
+                    methodPower = threadCpuTimePercentages.get(threadEntry.getKey().getId()) * (methodEntry.getValue() / totalEncounters);
                 }
 
                 // Only of consumption evolution tracking is enabled
@@ -442,7 +441,7 @@ public class MonitoringHandler implements Runnable {
             for (var callTreeEntry : entry.getValue().entrySet()) {
                 double stackTracePower = 0.0;
                 if (totalEncounters >= Double.MIN_VALUE) {
-                    stackTracePower = threadCpuTimePercentages.get(entry.getKey().threadId())
+                    stackTracePower = threadCpuTimePercentages.get(entry.getKey().getId())
                             * (callTreeEntry.getValue() / totalEncounters);
                 }
 
